8c3a984d3889ca39689de906c6b603c6fdb1bcad
Hasan <brt.hshami@cma-cgm.com>
2022-03-17 14:19:23 +0200

    Automate player list number + add functionality for update on Google Sheets

---
+++ main_sheets_gspread.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import gspread
+from df2gspread import df2gspread as d2g
+from df2gspread import gspread2df as g2d
+import gspread_dataframe as gd
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'jsonFileFromGoogle.json', scope)
+gc = gspread.authorize(credentials)
+
+spreadsheet_key = '1u40di-ObKV-K0zBD3DHOAZJrBAWLnKe-8Cck6xD48_8'
+wks_name = 'Master'
+
+
+def download_sheet():
+    # Download df
+    dfd = g2d.download(spreadsheet_key, wks_name, credentials=credentials, col_names=True)
+    return dfd
+
+
+def upload_sheet(df):
+    # Upload existing data
+    d2g.upload(df, spreadsheet_key, wks_name, credentials=credentials, row_names=False, col_names=True)
